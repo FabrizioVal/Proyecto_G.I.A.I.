e677670f675568c7ff7e5bee601fe72e9421ab68
FabriVal <fabriziogabrielvallone@gmail.com>
2024-04-26 09:48:51 -0300

    26 04 2024 Arreglos sencillos.

--- root/frontend/src/components/Inventory.tsx
+++ root/frontend/src/components/Inventory.tsx
@@ -1,13 +1,36 @@
-import Product from './ProductContainer';
+import { useState, useEffect } from 'react';
+import ProductContainer from './ProductContainer';
+import axios from 'axios';
 
 const Inventory = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch product data from backend API
+    axios.get('http://localhost:3000/api/products/inventario')
+      .then(response => {
+        // Access the data property of the AxiosResponse object
+        setProducts(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching product data:', error);
+      });
+  }, []);
+
+  
+
   return (
-    <div>
-        
-        <Product/>
-        
+    <div className="flex flex-wrap justify-center mt-10">
+      {products.map(product => (
+        <ProductContainer
+          imageUrl={product.imageUrl} // Assuming product.imageUrl contains the image URL
+          name={product.name}
+          price={product.price}
+          quantity={product.quantity}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Inventory
+export default Inventory;
--- root/frontend/src/components/ProductContainer.tsx
+++ root/frontend/src/components/ProductContainer.tsx
@@ -1,33 +1,23 @@
-
-import imageUrl from "../assets/product1.png";
 import {
   Card,
-
   CardFooter,
   CardHeader,
-  
 } from "@material-tailwind/react";
 
-const ProductContainer = () => {
-  const name = '';
-  const price = '';
-  const quantity = '';
-
+const ProductContainer = ({ imageUrl, name, price, quantity }) => {
   return (
-    <div className="max-w-xs p-6"> {/* Apply max-w-xl class to limit the width */}
+    <div className="max-w-xs p-6">
       <Card className="bg-gray-300">
         <CardHeader>
-      
-          <div className=" justify-center flex w-full h-full">
-            <img  src={imageUrl} alt={name} />
+          <div className="justify-center flex w-full h-full">
+            <img src={imageUrl} alt={name} />
           </div>
         </CardHeader>
-        
         <CardFooter className="flex justify-center">
-        <div className="mt-4 -mb-2">
+          <div className="mt-4 -mb-2">
             <h3 className="text-left font-bold mb-2">{name}</h3>
             <div className="flex">
-              <p  style={{ fontSize: '30px' }} className="mr-4 text-">${price}</p>
+              <p style={{ fontSize: '30px' }} className="mr-4 text-">${price}</p>
               <p className="mt-2 mb-2 mr-2">Quantity: {quantity}</p>
             </div>
           </div>
