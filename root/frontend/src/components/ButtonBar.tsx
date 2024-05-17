1e59d3a290269b903f983b1ea068dee0fc43a5b6
FabriVal <fabriziogabrielvallone@gmail.com>
2024-05-17 11:35:59 -0300

    17 05 2024

--- root/frontend/src/components/ButtonBar.tsx
+++ root/frontend/src/components/ButtonBar.tsx
@@ -2,8 +2,14 @@
 Estas funciones de frontend estan en la carpeta de controllers. Cada boton tiene una funcion diferente. */ 
 
 import React, { ChangeEvent } from 'react';
-import { Button, Input } from "@material-tailwind/react";
+import { Button, 
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Input, } from "@material-tailwind/react";
 import { FaSearch } from "react-icons/fa";
+import { IoIosArrowUp } from "react-icons/io";
 import { sendProduct } from '../controllers/LocalProduct';
 
 interface HeaderProps {
@@ -21,10 +27,13 @@ const Header: React.FC<HeaderProps> = ({ setSearchQuery }) => {
     setOpen(true);
   };
 
+  const [openMenu, setOpenMenu] = React.useState(false);
+  const [openMenu1, setOpenMenu1] = React.useState(false);
+
   return (
     <div>
       <div className="flex justify-center">
-        <div className="relative flex w-full max-w-[24rem]">
+        <div className="relative flex w-full max-w-[20rem]">
           <Input
             type="text"
             label="Buscar un producto"
@@ -32,14 +41,75 @@ const Header: React.FC<HeaderProps> = ({ setSearchQuery }) => {
             className="pr-20"
             containerProps={{ className: "min-w-0" }}
           />
-          <Button size="sm" className="!absolute right-1 top-1 bottom-1 rounded">
-            <FaSearch className="w-4 h-4" />
-          </Button>
+          
+             <FaSearch className="w-5 h-5 !absolute rounded right-2 top-1/2 transform -translate-y-1/2 text-gray-400" />
+         
         </div>
         <Button style={{ borderColor: '#ff0000' }} onClick={handleButtonClick}>
           Añadir producto
         </Button>
-        <Button>etiquetas</Button>
+
+
+         <Menu placement="right-start">
+      <MenuHandler>
+        <Button>Etiquetas</Button>
+      </MenuHandler>
+      <MenuList>
+        
+        
+        <Menu
+          placement="right-start"
+          open={openMenu}
+          handler={setOpenMenu}
+          allowHover
+          offset={15}
+        >
+          <MenuHandler className="flex items-center justify-between">
+            <MenuItem>
+              Cantidad
+              <IoIosArrowUp
+                strokeWidth={2.5}
+                className={`h-3.5 w-3.5 transition-transform ${
+                  openMenu ? "rotate-90" : ""
+                }`}
+              />
+            </MenuItem>
+          </MenuHandler>
+          <MenuList>
+            <MenuItem>Mayor a menor</MenuItem>
+            <MenuItem>Menor a mayor</MenuItem>
+          </MenuList>
+        </Menu>
+
+        <Menu
+          placement="right-start"
+          open={openMenu1}
+          handler={setOpenMenu1}
+          allowHover
+          offset={15}
+        >
+          <MenuHandler className="flex items-center justify-between">
+            <MenuItem>
+              Precio
+              <IoIosArrowUp
+                strokeWidth={2.5}
+                className={`h-3.5 w-3.5 transition-transform ${
+                  openMenu1 ? "rotate-90" : ""
+                }`}
+              /> 
+            </MenuItem>
+          </MenuHandler>
+          <MenuList>
+            <MenuItem>Mayor a menor</MenuItem>
+            <MenuItem>Menor a mayor</MenuItem>
+          </MenuList>
+        </Menu>
+
+      </MenuList>
+    </Menu>
+
+
+        
       </div>
       {dialog}
     </div>
